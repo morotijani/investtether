50471c9a7a44a51611c85a8f5429aefad7b02454
Tijani Moro <tjhackx111@gmail.com>
2025-06-21 15:22:53 +0000

    initial commit

--- xd192/update.transaction.php
+++ xd192/update.transaction.php
@@ -6,9 +6,18 @@
         $status = sanitize($_POST['status']);
         $income_statement = sanitize($_POST['income_statement']);
 
+        // check if transaction exists
+        $statement = $dbConnection->prepare("SELECT * FROM xpto_transactions WHERE transaction_id =?");
+        $statement->execute([$id]);
+        $count_row = $statement->rowCount();
+        if ($count_row == 0) {
+            echo "Transaction does not exist";
+            exit;
+        }
+
         // update transaction status in database
-        $statement = $dbConnection->prepare("UPDATE xpto_transactions SET transaction_status = ? WHERE transaction_id = ?, transaction_income_statement = ?");
-        $statement->execute([$status, $id, $income_statement]);
+        $statement = $dbConnection->prepare("UPDATE xpto_transactions SET transaction_status = ?, transaction_income_statement = ? WHERE transaction_id = ?");
+        $statement->execute([$status, '1', $id]);
 
         echo "Transaction status updated";
     }
